f2724a30137960c670f74d8d8a27fff66f0b2605
Tim White <fignewtonfood@gmail.com>
2015-09-18 09:08:32 -0700

    add constructors to js for pizza and toppings objects, passes first two tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+function Pizza(quantity, price, toppings){
+    this.quantity = quantity;
+    this.price = price;
+    this.toppings = [];
+}
+
+function Toppings(bacon, greenPepper, mushroom, olive, onion, pepperoni, sausage, tomato) {
+    this.bacon = bacon;
+    this.greenPepper = greenPepper;
+    this.mushroom = mushroom;
+    this.olive = olive;
+    this.onion = onion;
+    this.pepperoni = pepperoni;
+    this.sausage = sausage;
+    this.tomato = tomato;
+}
 
 
 //jQuery
