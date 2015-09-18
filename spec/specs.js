65698c54a4d17051d0153dcfd4cfec78a624c345
Tim White <fignewtonfood@gmail.com>
2015-09-18 09:02:42 -0700

    add specs for constructors

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,22 @@
-describe('Contact', function() {
-    it('creates a new contact with the given specifications', function() {
-        var testContact = new Contact('Rita', 'Moreno');
-        expect(testContact.firstName).to.equal('Rita');
-        expect(testContact.lastName).to.equal('Moreno');
-        expect(testContact.addresses).to.eql([]);
+describe('Pizza', function() {
+    it('creates a new pizza with the given specifications', function() {
+        var testPizza = new Pizza(1, 0);
+        expect(testPizza.quantity).to.equal(1);
+        expect(testPizza.price).to.equal(0);
+        expect(testPizza.toppings).to.eql([]);
+    });
+});
+
+describe('Topping', function() {
+    it('creates a new set of toppings with the given specifications', function() {
+        var testToppings = new Toppings(1, 1, 1, 1, 1, 1, 1, 1);
+        expect(testToppings.bacon).to.equal(1);
+        expect(testToppings.greenPepper).to.equal(1);
+        expect(testToppings.mushroom).to.equal(1);
+        expect(testToppings.olive).to.equal(1);
+        expect(testToppings.onion).to.equal(1);
+        expect(testToppings.pepperoni).to.equal(1);
+        expect(testToppings.sausage).to.equal(1);
+        expect(testToppings.tomato).to.equal(1);
     });
 });
